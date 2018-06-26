53c07b24fa4300affde804da45b593ba8de56448
Quang Nguyen <quangnd.edu@gmail.com>
2018-06-26 10:01:16 +0700

    Add ios platform

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -3,22 +3,62 @@ import { withTracker } from 'meteor/react-meteor-data';
  
 import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
- 
+import ReactDOM from 'react-dom';
+
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      taskInput: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      taskInput: event.target.value
+    })
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    //const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    const text = this.state.taskInput;
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+    
+    this.setState({
+      taskInput: ''
+    })
+    // Clear form
+    //ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
- 
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List 😈</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              value={this.state.taskInput} onChange={this.handleChange}
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
- 
+        
         <ul>
           {this.renderTasks()}
         </ul>
@@ -29,6 +69,6 @@ class App extends Component {
 
 export default withTracker(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   };
 })(App);
